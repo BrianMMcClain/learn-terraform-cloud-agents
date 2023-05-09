66cfd3b766e2080d47b9d6b80a34df0ee52c8d23
Brian McClain <brianmmcclain@gmail.com>
2023-05-09 09:24:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.name
+  image = docker_image.nginx.image_id
   name  = "nginx"
   ports {
     internal = 80
